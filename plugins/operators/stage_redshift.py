bbf9547594d7200ddcf2b7e417cbaf3e376f8c5c
KarthikeyanKandan <akashkarthiofficial@gmail.com>
2021-09-19 01:04:11 +0530

    stage_redshift operator fixes

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,4 +1,5 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
@@ -7,23 +8,19 @@ class StageToRedshiftOperator(BaseOperator):
     template_fields = ('s3_key')
     copy_sql = """
         COPY {}
-        FROM {}
-        ACCESS_KEY_ID {}
-        SECRET_ACCESS_KEY {}
-        REGION {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        REGION '{}'
         TIMEFORMAT AS 'epochmillisecs'
-        TRUNCATECOLUMNS
-        BLANKASNULL
-        EMPTYASNULL
+        TRUNCATECOLUMNS BLANKASNULL EMPTYASNULL
         {} 'auto'
         {}
      """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 # operators params (with defaults)
                  redshift_conn_id = "",
                  aws_credentials_id = "",
                  table = "",
@@ -34,9 +31,8 @@ class StageToRedshiftOperator(BaseOperator):
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        # Mapped params
+
         self.redshift_conn_id = redshift_conn_id
         self.aws_credentials_id = aws_credentials_id
         self.table = table
@@ -49,13 +45,13 @@ class StageToRedshiftOperator(BaseOperator):
 
     def execute(self, context):
         """
-            Copy data from S3 buckets to staging tables in redshift cluster.
+            Copy data from S3 buckets to staging tables in a redshift cluster.
                 - redshift_conn_id: redshift cluster connection id
                 - aws_credentials_id: AWS connection credentials
-                - table: redshift cluster table name
+                - table: table name in redshift cluster
                 - s3_bucket: S3 bucket name of source data
                 - s3_key: S3 key files of source data
-                - file_format: source file format - options JSON, CSV
+                - file_format: source file format JSON or CSV
         """
         aws_hook = AwsHook(self.aws_credentials_id)
         credentials = aws_hook.get_credentials()
@@ -65,30 +61,30 @@ class StageToRedshiftOperator(BaseOperator):
         redshift.run("DELETE FROM {}".format(self.table))
 
         self.log.info('Copy data from S3 to Redshift')
-        s3_path = "s3.//{}".format(self.s3_bucket)
+        s3_path = "s3://{}".format(self.s3_bucket)
         #Backfill to certain date
         if self.execution_date:
             year = self.execution_date.strftime("%Y")
             month = self.execution_date.strftime("%m")
             day = self.execution_date.strftime("%d")
-            s3_path = "/".join(s3_path, str(year), str(month), str(day))
+            s3_path = "/".join([s3_path, str(year), str(month), str(day)])
         s3_path = s3_path + "/" + self.s3_key
 
-        additional = ""
-        if file_format=="CSV":
-            additional = " DELIMITER ',' IGNOREHEADER 1 "
+        to_csv = ""
+        if self.file_format == "CSV":
+            to_csv = " DELIMITER ',' IGNOREHEADER 1 "
         
-        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+        copy_sql_formatted = StageToRedshiftOperator.copy_sql.format(
             self.table,
             s3_path,
             credentials.access_key,
             credentials.secret_key,
             self.region,
             self.file_format,
-            additional
+            to_csv
         )
 
-        redshift.run(formatted_sql)
+        redshift.run(copy_sql_formatted)
         self.log.info('Success: Copied data from {self.s3_bucket} S3 bucket to {self.table} staging table in Redshift')
 
 
