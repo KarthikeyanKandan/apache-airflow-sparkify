a92e038a7d565c43d8e3845d1e04cbe59fd9f6a0
KarthikeyanKandan <akashkarthiofficial@gmail.com>
2021-09-19 01:33:15 +0530

    parameters added to load fact operator

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -11,12 +11,19 @@ class LoadFactOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id = "",
+                 aws_credentials_id = "",
+                 sql_query = "",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
         # Map params here
         # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.sql_query = sql_query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id = self.redshift_conn_id)
+        redshift.run(str(self.sql_query))
+        self.log.info('Success: Data moved from Staging to Fact table')
